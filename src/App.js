881e00df1465275aceaadc273cafe9c553672f28
Jorge Dias <jorgedsdias@gmail.com>
2019-05-21 20:37:08 -0300

    Correção espaçamento

--- src/App.js
+++ src/App.js
@@ -15,32 +15,32 @@ import './js/startmin.js';
 import Menu from './components/Menu';
 
 class App extends Component {
-	componentDidMount() {
-		localStorage.setItem('msg', '');
-		if(localStorage.getItem('x-access-token') === null) {
-			localStorage.setItem('msg', 'You can\'t access this address');
-			this.props.children.props.history.push('/login');
-		}
+    componentDidMount() {
+        localStorage.setItem('msg', '');
+        if(localStorage.getItem('x-access-token') === null) {
+            localStorage.setItem('msg', 'You can\'t access this address');
+            this.props.children.props.history.push('/login');
+        }
 
-		$('#side-menu').metisMenu();
-	}
+        $('#side-menu').metisMenu();
+    }
 
-	componentDidUpdate() {
-		$('#side-menu').metisMenu();
-	}
+    componentDidUpdate() {
+        $('#side-menu').metisMenu();
+    }
 
-	render() {
-		return (
-			<div id="root">
-				<div id="wrapper">
-					<Menu />
-					<div id="page-wrapper">
-						{this.props.children}
-					</div>
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div id="root">
+                <div id="wrapper">
+                    <Menu />
+                    <div id="page-wrapper">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
